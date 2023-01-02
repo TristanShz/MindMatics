1462ad279208c5313b8cb4a40f60cffdfe93116e
TristanSch1 <tristan.sch57@gmail.com>
2023-01-02 18:59:46 +0100

    Fixed MongoDB connection

--- server/config/db.config.js
+++ server/config/db.config.js
@@ -1,10 +1,10 @@
-const mongoose = require("mongoose");
+const mongoose = require("mongoose")
 
 async function connect() {
   mongoose
     .connect(
-      "mongodb+srv://natix:Mttz4fr5jrjC9Q8@genshinproject.jrr5l.mongodb.net/genshinProject?retryWrites=true&w=majority",
-      { useNewUrlParser: true, useUnifiedTopology: true }
+      "mongodb+srv://mindmatics_user:fetl6HwD7aEVYbsH@cluster0.6jcpanv.mongodb.net/?retryWrites=true&w=majority",
+      { useNewUrlParser: true }
     )
     .then(() => console.log("Connexion à MongoDB réussie !"))
     .catch(() => console.log("Connexion à MongoDB échouée !"));
--- server/index.js
+++ server/index.js
@@ -1,7 +1,10 @@
 const http = require("http");
 const app = require("./app");
-const port = 5000;
+
+const port = 3001;
+
 app.set("port", port);
+
 const server = http.createServer(app);
 
 server.listen(port);
