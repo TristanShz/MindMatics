0a23e973d3193ee0b8ce5f24893c83d2c8315a69
Haorsis <cadetgauthier3@gmail.com>
2023-01-04 11:17:05 +0100

    rework api architecture to add test folder

--- api/src/routes/users.js
+++ api/src/routes/users.js
@@ -2,17 +2,11 @@ const express = require("express");
 const usersController = require("../controllers/users");
 const router = express.Router();
 
-router.get("/", (req, res) => {
-  res.send("Hello World!");
-});
+router.get("/", usersController.retrieveAll);
 
 router.post("/register", usersController.register);
 
 router.post("/login", usersController.login);
-
-router.post("/getAllUsers", usersController.retrieveAll);
-
-router.get("/getUserByPseudo", usersController.retrieveByUsername);
 
 router.post("/getAllUsers", usersController.retrieveAll);
 
