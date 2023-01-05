5397556192656fe2d704c58590a6e5fed5d3db62
Haorsis <cadetgauthier3@gmail.com>
2023-01-05 10:16:50 +0100

    result controller

--- api/controllers/results.js
+++ api/controllers/results.js
@@ -8,9 +8,9 @@ const utilsService = require("../services/utilsService");
  * @param {*} res
  */
 exports.getAll = async (req, res) => {
-    const results = await resultsService.getAll();
-    return res.status(200).json({data: results});
-}
+  const results = await resultsService.getAll();
+  res.status(200).json({ data: results });
+};
 
 /**
  * create Result
@@ -18,24 +18,29 @@ exports.getAll = async (req, res) => {
  * @param {*} res
  */
 exports.create = async (req, res) => {
-    if (!req.body.username || !req.body.score || !req.body.difficulty){
-        return res.status(400).json({error: 'At least one field is empty'});
-    }
-    const loggedUser = await usersService.getUserByUsername(req.body.username);
-    if(!loggedUser){
-        return res.status(404).json({error: 'No user matching id'});
-    }
-    const resultToSave = {
-        user: loggedUser._id,
-        score: req.body.score,
-        difficulty: req.body.difficulty,
-    }
-    const createdResult = await resultsService.create(resultToSave);
-    if(!createdResult){
-        return res.status(500).json({error: 'error while creating result'});
-    }
-    return res.status(200).json({data: createdResult});
-}
+  if (!req.body.username || !req.body.score || !req.body.difficulty) {
+    res.status(400).json({ error: "At least one field is empty" });
+    return;
+  }
+  const loggedUser = await usersService.getUserByUsername(req.body.username);
+
+  if (!loggedUser) {
+    res.status(404).json({ error: "No user matching username" });
+    return;
+  }
+  const resultToSave = {
+    user: loggedUser._id,
+    score: req.body.score,
+    difficulty: req.body.difficulty,
+  };
+
+  const createdResult = await resultsService.create(resultToSave);
+  if (!createdResult) {
+    res.status(500).json({ error: "error while creating result" });
+    return;
+  }
+  res.status(201).json({ data: createdResult });
+};
 
 /**
  * remove result using his id
@@ -43,15 +48,12 @@ exports.create = async (req, res) => {
  * @param {*} res
  */
 exports.remove = async (req, res) => {
-    if (!utilsService.checkObjectId(req.body.resultId)){
-        return res.status(400).json({error: 'ObjectId is not valid'});
-    }
-    const resultToDelete = await resultsService.get(req.body.resultId);
-    if(!resultToDelete){
-        return res.status(404).json({error: 'No result matching id'});
-    }
-    resultsService.remove(resultToDelete);
-    return res.status(200);
+  if (!utilsService.checkObjectId(req.body.resultId)) {
+    res.status(400).json({ error: "ObjectId is not valid" });
+  }
+  const resultToDelete = await resultsService.get(req.body.resultId);
+  if (!resultToDelete) {
+    res.status(404).json({ error: "No result matching id" });
   }
   resultsService.remove(resultToDelete);
 };
